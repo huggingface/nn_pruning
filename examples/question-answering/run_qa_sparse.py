81f1bc916822c5e1834bb0a701ae2790885db617
François Lagunas <francois.lagunas@gmail.com>
2020-12-21 11:27:43 +0100

    Splitting parameters for attention and dense layers.

--- examples/question-answering/run_qa_sparse.py
+++ examples/question-answering/run_qa_sparse.py
@@ -1,20 +1,17 @@
-from nn_pruning.modules.masked_nn import (
-    LinearPruningModulePatcher,
-    LinearPruningParameters,
-    JointPruningModulePatcher,
-    ChannelPruningModulePatcher
-)
+from dataclasses import dataclass, field
 
-from nn_pruning.training_patcher import BertLinearModelPatcher, PatcherContext
-
+import torch.nn as nn
 from nn_pruning.model_structure import BertStructure
-from transformers.optimization import AdamW, get_linear_schedule_with_warmup
-
+from nn_pruning.modules.masked_nn import (
+    ChannelPruningModulePatcher,
+    JointPruningModulePatcher,
+    LinearPruningParameters,
+)
+from nn_pruning.training_patcher import BertLinearModelPatcher, PatcherContext
 from run_qa import QATraining
 from trainer_qa import QuestionAnsweringTrainer
-import torch.nn as nn
+from transformers.optimization import AdamW, get_linear_schedule_with_warmup
 
-from dataclasses import dataclass, field
 
 @dataclass
 class SparseTrainingArguments:
@@ -22,7 +19,9 @@ class SparseTrainingArguments:
     Sparse training specific arguments
     """
 
-    mask_scores_learning_rate: float = field(default=1e-2, metadata={"help": "The initial learning rate for mask_scores."})
+    mask_scores_learning_rate: float = field(
+        default=1e-2, metadata={"help": "The initial learning rate for mask_scores."}
+    )
 
     dense_pruning_method: str = field(
         default="topk", metadata={"help": "Dense Layers pruning method."}
@@ -33,7 +32,8 @@ class SparseTrainingArguments:
     )
 
     ampere_pruning_method: str = field(
-        default="disabled", metadata={"help": "Ampere sparse method ('disabled' for no ampere sparsity)"}
+        default="disabled",
+        metadata={"help": "Ampere sparse method ('disabled' for no ampere sparsity)"},
     )
 
     mask_init: str = field(
@@ -41,28 +41,37 @@ class SparseTrainingArguments:
     )
 
     mask_scale: float = field(
-        default=0.0, metadata={"help": "Parameter to use with mask_init."},
+        default=0.0,
+        metadata={"help": "Parameter to use with mask_init."},
     )
 
-    block_rows: int = field(
-        default=1, metadata={"help": "Block size in rows."},
+    dense_block_rows: int = field(
+        default=1,
+        metadata={"help": "Block size in rows for dense layers."},
     )
 
-    block_cols: int = field(
-        default=1, metadata={"help": "Block size in cols."},
+    dense_block_cols: int = field(
+        default=1,
+        metadata={"help": "Block size in cols for dense layers."},
+    )
+
+    attention_block_rows: int = field(
+        default=1,
+        metadata={"help": "Block size in rows for attention."},
+    )
+
+    attention_block_cols: int = field(
+        default=1,
+        metadata={"help": "Block size in cols for attention."},
     )
 
     initial_threshold: float = field(
         default=1.0,
-        metadata={
-            "help": "Initial value of the threshold (for scheduling)."
-        },
+        metadata={"help": "Initial value of the threshold (for scheduling)."},
     )
     final_threshold: float = field(
         default=0.5,
-        metadata={
-            "help": "Final value of the threshold (for scheduling)."
-        },
+        metadata={"help": "Final value of the threshold (for scheduling)."},
     )
 
     initial_warmup: float = field(
@@ -80,22 +89,19 @@ class SparseTrainingArguments:
 
     initial_ampere_temperature: float = field(
         default=0.0,
-        metadata={
-            "help": "Initial value of the ampere temperature (for scheduling)."
-        },
+        metadata={"help": "Initial value of the ampere temperature (for scheduling)."},
     )
     final_ampere_temperature: float = field(
         default=20.0,
-        metadata={
-            "help": "Final value of the ampere temperature (for scheduling)."
-        },
+        metadata={"help": "Final value of the ampere temperature (for scheduling)."},
     )
     final_lambda: float = field(
-        default=.0,
+        default=0.0,
         metadata={
             "help": "Regularization intensity (used in conjunction with `regularization`)."
         },
     )
+
 
 class QASparseTrainer(QuestionAnsweringTrainer):
     def __init__(self, sparse_args, *args, **kwargs):
@@ -104,33 +110,36 @@ class QASparseTrainer(QuestionAnsweringTrainer):
 
     def compute_loss(self, model, inputs):
         loss = super().compute_loss(model, inputs)
-        #loss += self.regularization(model)
+        # loss += self.regularization(model)
         return loss
 
     def regularization(model: nn.Module, mode: str):
         # TODO
-        assert(False)
+        assert False
         if mode not in ["l0", "l1"]:
             return 0
         regu, counter = 0, 0
         for name, module in model.named_modules():
-            #if isinstance(module, )
+            # if isinstance(module, )
 
             if "mask_scores" in name:
                 if mode == "l1":
                     regu += torch.norm(torch.sigmoid(param), p=1) / param.numel()
                 elif mode == "l0":
-                    regu += torch.sigmoid(param - 2 / 3 * np.log(0.1 / 1.1)).sum() / param.numel()
+                    regu += (
+                        torch.sigmoid(param - 2 / 3 * np.log(0.1 / 1.1)).sum()
+                        / param.numel()
+                    )
                 else:
                     ValueError("Don't know this mode.")
                 counter += 1
         return regu / counter
 
     def create_optimizer_and_scheduler(self, num_training_steps: int):
-        assert(self.optimizer is None)
+        assert self.optimizer is None
         self.optimizer = self.create_optimizer(self.model)
 
-        assert(self.lr_scheduler is None)
+        assert self.lr_scheduler is None
         self.lr_scheduler = self.create_scheduler(num_training_steps)
 
     def create_optimizer(self, model):
@@ -138,7 +147,7 @@ class QASparseTrainer(QuestionAnsweringTrainer):
         # Prepare optimizer and schedule (linear warmup and decay)
         no_decay = ["bias", "LayerNorm.weight"]
         args = self.args
-        sparse_args =  self.sparse_args
+        sparse_args = self.sparse_args
 
         mask_params = []
         no_decay_params = []
@@ -153,7 +162,6 @@ class QASparseTrainer(QuestionAnsweringTrainer):
                 no_decay_params.append(p)
             else:
                 decay_params.append(p)
-
 
         optimizer_grouped_parameters = [
             {
@@ -172,55 +180,62 @@ class QASparseTrainer(QuestionAnsweringTrainer):
             },
         ]
 
-        optimizer = AdamW(optimizer_grouped_parameters, lr=args.learning_rate, eps=args.adam_epsilon)
+        optimizer = AdamW(
+            optimizer_grouped_parameters, lr=args.learning_rate, eps=args.adam_epsilon
+        )
         return optimizer
 
-    def create_scheduler(self, num_training_steps:int):
+    def create_scheduler(self, num_training_steps: int):
         scheduler = get_linear_schedule_with_warmup(
-            self.optimizer, num_warmup_steps= self.args.warmup_steps, num_training_steps=num_training_steps
+            self.optimizer,
+            num_warmup_steps=self.args.warmup_steps,
+            num_training_steps=num_training_steps,
         )
         return scheduler
 
-from transformers.trainer_callback import (
-    TrainerCallback,
-)
 
-from transformers.trainer import (
-TrainingArguments,
-TrainerState,
-TrainerControl
-)
+from transformers.trainer import TrainerControl, TrainerState, TrainingArguments
+from transformers.trainer_callback import TrainerCallback
+
 
 class SchedulingCallback(TrainerCallback):
-    def __init__(self, context:PatcherContext, sparse_args:SparseTrainingArguments):
+    def __init__(self, context: PatcherContext, sparse_args: SparseTrainingArguments):
         self.context = context
         self.sparse_args = sparse_args
 
-    def on_step_begin(self, args: TrainingArguments, state: TrainerState, control: TrainerControl, **kwargs):
+    def on_step_begin(
+        self,
+        args: TrainingArguments,
+        state: TrainerState,
+        control: TrainerControl,
+        **kwargs
+    ):
         sparse_args = self.sparse_args
-        self.schedule_threshold(step=state.global_step,
-                                total_step=state.max_steps,
-                                warmup_steps=args.warmup_steps,
-                                initial_threshold=sparse_args.initial_threshold,
-                                final_threshold=sparse_args.final_threshold,
-                                initial_warmup=sparse_args.initial_warmup,
-                                final_warmup=sparse_args.final_warmup,
-                                final_lambda=sparse_args.final_lambda,
-                                initial_ampere_temperature=sparse_args.initial_ampere_temperature,
-                                final_ampere_temperature=sparse_args.final_ampere_temperature
-                                )
+        self.schedule_threshold(
+            step=state.global_step,
+            total_step=state.max_steps,
+            warmup_steps=args.warmup_steps,
+            initial_threshold=sparse_args.initial_threshold,
+            final_threshold=sparse_args.final_threshold,
+            initial_warmup=sparse_args.initial_warmup,
+            final_warmup=sparse_args.final_warmup,
+            final_lambda=sparse_args.final_lambda,
+            initial_ampere_temperature=sparse_args.initial_ampere_temperature,
+            final_ampere_temperature=sparse_args.final_ampere_temperature,
+        )
 
-    def schedule_threshold(self,
-            step: int,
-            total_step: int,
-            warmup_steps: int,
-            initial_threshold: float,
-            final_threshold: float,
-            initial_warmup: float,
-            final_warmup: float,
-            final_lambda: float,
-            initial_ampere_temperature: float,
-            final_ampere_temperature: float,
+    def schedule_threshold(
+        self,
+        step: int,
+        total_step: int,
+        warmup_steps: int,
+        initial_threshold: float,
+        final_threshold: float,
+        initial_warmup: float,
+        final_warmup: float,
+        final_lambda: float,
+        initial_ampere_temperature: float,
+        final_ampere_temperature: float,
     ):
         if step <= initial_warmup * warmup_steps:
             threshold = initial_threshold
@@ -231,9 +246,15 @@ class SchedulingCallback(TrainerCallback):
         else:
             spars_warmup_steps = initial_warmup * warmup_steps
             spars_schedu_steps = (final_warmup + initial_warmup) * warmup_steps
-            mul_coeff = 1 - (step - spars_warmup_steps) / (total_step - spars_schedu_steps)
-            threshold = final_threshold + (initial_threshold - final_threshold) * (mul_coeff ** 3)
-            ampere_temperature = final_ampere_temperature + (initial_ampere_temperature - final_ampere_temperature) * (mul_coeff ** 3)
+            mul_coeff = 1 - (step - spars_warmup_steps) / (
+                total_step - spars_schedu_steps
+            )
+            threshold = final_threshold + (initial_threshold - final_threshold) * (
+                mul_coeff ** 3
+            )
+            ampere_temperature = final_ampere_temperature + (
+                initial_ampere_temperature - final_ampere_temperature
+            ) * (mul_coeff ** 3)
 
         regu_lambda = final_lambda * threshold / final_threshold
 
@@ -256,29 +277,46 @@ class QASparseTraining(QATraining):
         self.trainer.add_callback(scheduling)
 
     def get_arguments(self):
-        return {"sparse":SparseTrainingArguments}
+        return {"sparse": SparseTrainingArguments}
+
+    def parse_pruning_method(self, method):
+        parts = method.split(":")
+        if len(parts) == 2:
+            return parts
+        elif len(parts) == 1:
+            return parts[0], "default"
+        else:
+            raise RuntimeError("Could not parse pruning method")
 
     def patch_model(self):
+        attention_pruning_method_parts = self.parse_pruning_method(self.sparse_args.attention_pruning_method)
+
         parameters_attention = LinearPruningParameters(
-            method="topK",
-            submethod="default",
-            ampere_method="annealing",
-            block_rows=64,
-            block_cols=768,
+            method=attention_pruning_method_parts[0],
+            submethod=attention_pruning_method_parts[1],
+            ampere_method=self.sparse_args.ampere_pruning_method,
+            block_rows=self.sparse_args.attention_block_rows,
+            block_cols=self.sparse_args.attention_block_cols,
         )
 
+        dense_pruning_method_parts = self.parse_pruning_method(self.sparse_args.dense_pruning_method)
+
         parameters_dense = LinearPruningParameters(
-            method="topK",
-            submethod="1d",
-            ampere_method="disabled",
-            block_rows=32,
-            block_cols=32,
+            method=dense_pruning_method_parts[0],
+            submethod=dense_pruning_method_parts[1],
+            ampere_method=self.sparse_args.ampere_pruning_method,
+            block_rows=self.sparse_args.dense_block_rows,
+            block_cols=self.sparse_args.dense_block_cols,
         )
 
         patcher_context = self.patcher_context
 
-        p_attention = JointPruningModulePatcher(patcher_context, parameters_attention, suffix=".attention")
-        p_dense = ChannelPruningModulePatcher(patcher_context, parameters_dense, BertStructure, suffix="dense")
+        p_attention = JointPruningModulePatcher(
+            patcher_context, parameters_attention, suffix=".attention"
+        )
+        p_dense = ChannelPruningModulePatcher(
+            patcher_context, parameters_dense, BertStructure, suffix="dense"
+        )
 
         module_patchers = dict(
             query=p_attention,
@@ -292,14 +330,14 @@ class QASparseTraining(QATraining):
         patcher = BertLinearModelPatcher(module_patchers)
         patcher.patch(self.model)
 
-        assert(patcher.stats["patched"] == 72)
+        assert patcher.stats["patched"] == 72
         if False:
             key_sizes = {k: len(v) for k, v in patcher_context.context_modules.items()}
 
             for k, v in key_sizes.items():
                 print(k, v)
 
-            assert(key_sizes ==  {"ampere_mask": 72, "mask": 12, 'mask_1d': 48})
+            assert key_sizes == {"ampere_mask": 72, "mask": 12, "mask_1d": 48}
 
     def create_model(self):
         super().create_model()
@@ -310,12 +348,15 @@ class QASparseTraining(QATraining):
         self.patcher_context.set_context_data("ampere_temperature", 0.0)
         super().train()
 
+
 def main():
     QASparseTraining.run_from_command_line()
+
 
 def _mp_fn(index):
     # For xla_spawn (TPUs)
     main()
+
 
 if __name__ == "__main__":
     main()
