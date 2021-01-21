7a254a67f4019e24df5a09b29237d7b378b6ceb9
Francois Lagunas <francois.lagunas@gmail.com>
2021-01-21 16:57:33 +0100

    new launch script for final fine tuning

---
+++ nn_pruning/examples/question_answering/launch_qa_sparse_single.py
@@ -0,0 +1,26 @@
+import sys
+import copy
+
+def main():
+    import json
+    from pathlib import Path
+
+    if len(sys.argv) < 2:
+        raise RuntimeError("Please specify json file")
+    filename = Path(sys.argv[1]).resolve()
+    param_dict = json.load(open(filename))
+
+    import nn_pruning.examples.question_answering.qa_sparse_xp as qa_sparse_xp
+
+    qa = qa_sparse_xp.QASparseXP(param_dict)
+    qa.run()
+
+
+
+def _mp_fn(index):
+    # For xla_spawn (TPUs)
+    main()
+
+
+if __name__ == "__main__":
+    main()
