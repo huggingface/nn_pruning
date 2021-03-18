f103e81a164633d24d531341156c62cf9c765b4b
lewtun <lewis.c.tunstall@gmail.com>
2021-03-18 14:28:28 +0100

    Add text classification notebook (#6)
    * Add dependencies to setup
    
    * Add skeleton notebook for sparse trainer
    
    * Ignore Jupyter notebook checkpoints
    
    * Fine-prune a baseline model
    
    * Add matplotlib dependency
    
    * Add inference text
    
    * Add pandas as dependency for examples
    
    * Clean up sparse trainer notebook
    
    * Rename notebook
    
    * Remove deleted notebook
    
    * Remove redundant hyperparams
    
    * Clarify comparison between fine-pruned and unpruned BERT-base accuracy

--- setup.py
+++ setup.py
@@ -5,11 +5,13 @@ def readme():
     with open("README.md") as f:
         return f.read()
 
+extras = {}
+extras["examples"] = ["numpy>=1.2.0", "datasets>=1.4.1", "ipywidgets>=7.6.3", "matplotlib>=3.3.4", "pandase>=1.2.3"]
 
 setup(
     name="nn_pruning",
     version="0.1",
-    description="nn_pruning is a python package for ...",
+    description="nn_pruning is a Python package for pruning Transformers",
     long_description=readme(),
     classifiers=[
         "Development Status :: 3 - Alpha",
@@ -23,7 +25,8 @@ setup(
     author_email="",
     license="MIT",
     packages=["nn_pruning"],
-    install_requires=["click"],
+    install_requires=["click", "transformers>=4.3.0", "torch>=1.6", "scikit-learn>=0.24"],
+    extras_require=extras,
     test_suite="nose.collector",
     tests_require=["nose", "nose-cover3"],
     entry_points={
