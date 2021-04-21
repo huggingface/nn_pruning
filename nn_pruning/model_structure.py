48eb0a2fc64fa418d508903fa09eae0dc335f98d
Ella Charlaix <ella@huggingface.co>
2021-04-21 09:14:01 +0200

    Modification to make inference model patcher structure independent.

--- nn_pruning/model_structure.py
+++ nn_pruning/model_structure.py
@@ -1,5 +1,6 @@
 from typing import Dict
 import re
+from transformers import BertConfig, BartConfig
 
 class ModelStructure:
     PATTERN_PREFIX: str = ""
@@ -16,10 +17,21 @@ class ModelStructure:
         raise RuntimeError(f"module name {module_name} does not match patterns {LAYER_PATTERNS_STRING}")
 
     @classmethod
-    def is_attention(cls, module_name):
-        pos, _ = cls.get_module_intra_layer_position(module_name)
-        category = list(cls.LAYER_PATTERNS.keys())[pos]
-        return category in cls.ATTENTION_LAYERS
+    def is_attention(cls, module_name, exclude_att_dense=False):
+        patterns = cls.ATTENTION_LAYERS if exclude_att_dense else cls.MHA_LAYERS
+        for i, pattern in enumerate(patterns):
+            if cls.LAYER_PATTERNS[pattern] in module_name:
+                return True
+        return False
+
+    @classmethod
+    def is_ffn(cls, module_name):
+        if cls.is_attention(module_name, exclude_att_dense=False):
+            return False
+        for i, pattern in enumerate(cls.FFN_LAYERS):
+            if cls.LAYER_PATTERNS[pattern] in module_name:
+                return True
+        return False
 
     @classmethod
     def layer_index(cls, child_module_name):
@@ -39,8 +51,10 @@ class BertStructure(ModelStructure):
         interm_dense="intermediate.dense",
         output_dense="output.dense",
     )
+    ATTENTION_PREFIX = ("attention.self",)
     ATTENTION_LAYERS = ("query", "key", "value")
-
+    MHA_LAYERS = ATTENTION_LAYERS + ("att_dense",)
+    FFN_LAYERS = ("interm_dense", "output_dense")
 
 class BartStructure(ModelStructure):
     PATTERN_PREFIX = "model.(en|de)coder.layers.[0-9]+."
@@ -56,4 +70,45 @@ class BartStructure(ModelStructure):
         interm_dense="fc1",
         output_dense="fc2",
     )
+    ATTENTION_PREFIX = ("self_attn", "encoder_attn")
     ATTENTION_LAYERS = ("query", "key", "value", "encoder_decoder_query", "encoder_decoder_key", "encoder_decoder_value")
+    MHA_LAYERS = ATTENTION_LAYERS + ("att_dense", "encoder_decoder_att_dense")
+    FFN_LAYERS = ("interm_dense", "output_dense")
+
+
+config2struct = {
+    BertConfig: BertStructure,
+    BartConfig: BartStructure
+}
+
+name2struct = {
+    "bert": BertStructure,
+    "bart": BartStructure
+}
+
+def struct_from_config(model):
+    structure = config2struct.get(model.config_class, None)
+    if structure is None:
+        raise RuntimeError(f"Model config does not match any of the defined structures.")
+    return structure
+
+def struct_from_name(model_name):
+    for name in name2struct.keys():
+        if name in model_name:
+            return name2struct[name]
+    raise RuntimeError(f"Model name does not match any of the defined structures.")
+
+def count_num_heads(model):
+    head_count = 0
+    model_structure = struct_from_config(model)
+    for name, module in model.named_modules():
+        for attention_prefix in model_structure.ATTENTION_PREFIX:
+            if name.endswith(attention_prefix):
+                if hasattr(module, 'num_attention_heads'):
+                    num_attention_heads = module.num_attention_heads
+                elif hasattr(module, 'num_heads'):
+                    num_attention_heads = module.num_heads
+                else:
+                    raise RuntimeError(f"Not able to retrieve number of attention head")
+                head_count += num_attention_heads
+    return head_count
